8d910dd6738b5dc309d164c67bc8edd9715c1c8b
zhangdong <38548490+zhangdddong@users.noreply.github.com>
2019-10-10 15:10:31 +0800

    day14

---
+++ ZD_LeetCode/19.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode R(-1);
+        R.next = head;
+        ListNode *p = &R, *q =  &R;
+        for (int i = 0; i < n; ++i) 
+            p = p->next;
+        while(p->next) {
+            p = p->next;
+            q = q->next;
+        }
+        ListNode *t = q->next;
+        q->next = t->next;
+        delete t;
+        return R.next;
+    }
+};
---
+++ ZD_LeetCode/24.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode R(-1);
+        R.next = head;
+        ListNode *p = &R, *q = p->next, *z = q->next;
+        while (z) {
+            p->next = z;
+            q->next = z->next;  
+            z->next = q;
+            p = q;
+            q = q->next;
+            z = q == 0 ? 0 : q->next;   
+        }
+        return R.next;
+    }
+};

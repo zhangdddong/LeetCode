dbfdf2dbf0174393dbb9ebdfc25f799a2459b0e8
zhangdong <38548490+zhangdddong@users.noreply.github.com>
2019-11-01 20:44:57 +0800

    day31

---
+++ ZD_LeetCode/107.cpp
@@ -0,0 +1,17 @@
+class Solution {
+	public:
+		vector<vector<int> > levelOrderBottom(TreeNode *root) {
+			vector<vector<int>> result;
+			traverse(root, 1, result);
+			std::reverse(result.begin(), result.end()); 
+			return result;
+		}
+		void traverse(TreeNode *root, size_t level, vector<vector<int>> &result) {
+			if (!root) return;
+			if (level > result.size())
+				result.push_back(vector<int>());
+			result[level-1].push_back(root->val);
+			traverse(root->left, level+1, result);
+			traverse(root->right, level+1, result);
+		}
+};

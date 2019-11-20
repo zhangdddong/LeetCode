91b0ee3a24e7d47c703757cd1e74b4245bacbf83
zhangdong <38548490+zhangdddong@users.noreply.github.com>
2019-11-20 10:53:31 +0800

    day36

---
+++ ZD_LeetCode/147.cpp
@@ -0,0 +1,21 @@
+class Solution {
+	public:
+		ListNode *insertionSortList(ListNode *head) {
+			ListNode dummy(INT_MIN);
+			for (ListNode *cur = head; cur != nullptr;) {
+				auto pos = findInsertPos(&dummy, cur->val);
+				ListNode *tmp = cur->next;
+				cur->next = pos->next;
+				pos->next = cur;
+				cur = tmp;
+			}
+			return dummy.next;
+		}
+		ListNode* findInsertPos(ListNode *head, int x) {
+			ListNode *pre = nullptr;
+			for (ListNode *cur = head; cur != nullptr && cur->val <= x;
+			        pre = cur, cur = cur->next)
+				;
+			return pre;
+		}
+};
